79e0b2a28d0911666ed749075b2a9bcc7a18603a
juhriber <j.de.s.r.25@gmail.com>
2021-03-08 15:15:45 -0300

    Finalização do projeto Cebolinha

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pasta_Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string frase;
+
+            //<---Inicio(Digite)--->//
+            Console.Write("Digite Qualquer Coisa: ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+} 
